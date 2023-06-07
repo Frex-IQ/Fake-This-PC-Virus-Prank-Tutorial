84768ca3a44c410cbdd29ec1dec069d4ba561264
Frex-IQ <135344857+Frex-IQ@users.noreply.github.com>
2023-06-07 21:27:29 +0530

    Add files via upload

---
+++ Fake This PC Virus Prank Tutorial/Fake This PC Virus Prank.bat
@@ -0,0 +1,7 @@
+@ECHO off
+:Begin
+msg * Error opening this computer. Do you want to fix it?
+msg * This error could not be corrected. Do you want to scan your computer?
+msg * Alert1 virus could not be detected.
+msg * Another party is trying to steal all the data on your computer
+GOTO BEGIN
